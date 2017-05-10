4c4c242e602892f6b6251d67dfea1bbce4642d37
gonpalace <gonpalace@gmail.com>
2017-05-10 18:41:11 +0900

    test bench generation codes are modified ; to prevent error from confusing of file 'mobs.py'

--- bench/dependency_ex/dependency_ex_gen.sh
+++ bench/dependency_ex/dependency_ex_gen.sh
@@ -1,12 +1,12 @@
 #rm -rf MainPrj_Repo Support_Repo
-mobs.py Module_A MainPrj_Repo
-mobs.py Module_B MainPrj_Repo
-mobs.py Module_C MainPrj_Repo
+../../mobs.py Module_A MainPrj_Repo
+../../mobs.py Module_B MainPrj_Repo
+../../mobs.py Module_C MainPrj_Repo
 
-mobs.py Module_D Support_Repo
-mobs.py Module_E Support_Repo
-mobs.py Module_F Support_Repo
-mobs.py Module_G Support_Repo
+../../mobs.py Module_D Support_Repo
+../../mobs.py Module_E Support_Repo
+../../mobs.py Module_F Support_Repo
+../../mobs.py Module_G Support_Repo
 patch -p0 < MainPrj_Repo.diff
 patch -p0 < Support_Repo.diff
 #cd MainPrj_Repo/Module_A ; scons ; cd -
